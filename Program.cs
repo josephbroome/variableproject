8711f8da92b4adcd757bb185dfb32226d7ea0a97
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-01 11:06:49 -0700

    changes

--- Program.cs
+++ Program.cs
@@ -7,5 +7,5 @@ double tennisBalls = 45;
 decimal weight = 42.2m;
 
 
-Console.WriteLine($"My dog's name is {dogName} but his nick name is {nickName}. He is {dogAge} years old and has {tennisBalls} tennis balls and it is {lovesFetching} that he loves to fetch, which helps to keep him at {weight} ");
+Console.WriteLine($"My dog's name is {dogName} but his nick name is {nickName}. He is {dogAge} years old and has {tennisBalls} tennis balls and it is {lovesFetching} that he loves to fetch, which helps to keep him at {weight} lbs ");
 
